453299b812500846f7e996b960e1e107d66548f7
Takuya Yonezawa <yonezawa.takuya.yq2@is.naist.jp>
2021-09-05 17:27:06 +0900

    up

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,39 +1,104 @@
 import Head from 'next/head'
-import Image from 'next/image'
+import { useReactMediaRecorder} from 'react-media-recorder';
 import styles from '../styles/Home.module.css'
-// import * as AWS from "@aws-sdk/client-transcribe";
+import React, {useState} from 'react';
 
-
-import { TranscribeClient, CreateCallAnalyticsCategoryCommand } from "@aws-sdk/client-transcribe";
-
-// const client = new Transcribe({ region: "ap-northeast-1" });
-const client = new TranscribeClient({ region: "ap-northeast-1" });
-
-// const transcribeservice = new AWS.TranscribeService();
-
-const params = {
-  /** input parameters */
-};
-
-const command = new CreateCallAnalyticsCategoryCommand(params);
-
+declare const window: Window &
+   typeof globalThis & {
+    MediaRecorder: any
+   };
 
 export default function Home() {
+
+  const {
+    status,
+    startRecording,
+    stopRecording,
+    resumeRecording,
+    pauseRecording,
+    mediaBlobUrl,
+  } = useReactMediaRecorder({ audio: true });
+
+  const [isNowRecording, setIsNowRecording] = useState(false)
+
+
+  function onStart() {
+    startRecording()
+    setIsNowRecording(true)
+  }
+  function onPause() {
+    pauseRecording()
+  }
+  function onResume() {
+    resumeRecording()
+  }
+  function onStop() {
+    stopRecording()
+    setIsNowRecording(false)
+  }
+
+
   return (
-   <>
-   <button  onClick={() => {
-     // async/await.
-try {
-  const data = await client.send(command);
-  // process data.
-} catch (error) {
-  // error handling.
-} finally {
-  // finally.
-}
-     
-     
-     }}>ぼたーん</button>
-   </>
+    <div className={styles.container}>
+      <Head>
+        <script>
+          {
+            () => {
+              // CSRでscriptを実行するために行う
+              if (!window.MediaRecorder) {
+                document.write(
+                  decodeURI('%3Cscript defer src="/polyfill.js">%3C/script>')
+                )
+              }
+            }
+          }
+        </script>
+
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className={styles.main}>
+        <h1 className={styles.title}>
+          Welcome to <a href="https://nextjs.org">Next.js!</a>
+        </h1>
+
+
+        <p className={styles.description}>
+          Get started by editing{' '}
+          <code className={styles.code}>pages/index.js</code>
+        </p>
+
+        <div className={styles.grid}>
+          <div className={styles.card}>
+            <button onClick={onStart} type="button" className="m-1">Record</button>
+            <button onClick={onPause} type="button" className="m-1">Pause</button>
+            <button onClick={onResume} type="button" className="m-1">Resume</button>
+            <button onClick={onStop} type="button" className="m-1">Stop</button>
+          </div>
+          <div className={styles.card}>
+
+            <audio src={mediaBlobUrl? mediaBlobUrl:''} controls />
+          </div>
+
+
+
+          <div className={styles.card}>
+            {isNowRecording ? <p>録音中</p> : <p>録音可能</p>}
+          </div>
+        </div>
+      </main>
+
+      <footer className={styles.footer}>
+        <a
+          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Powered by{' '}
+          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
+        </a>
+      </footer>
+    </div>
   )
 }
